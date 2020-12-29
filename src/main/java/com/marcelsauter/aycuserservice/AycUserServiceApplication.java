d9fac3ff1a96780973f71de027e6d016f42c6522
Sautino <marcelsauter9299@gmail.com>
2020-12-29 22:55:32 +0100

    inital commit

---
+++ src/main/java/com/marcelsauter/aycuserservice/AycUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcelsauter.aycuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AycUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AycUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelsauter/aycuserservice/AycUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelsauter.aycuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AycUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
