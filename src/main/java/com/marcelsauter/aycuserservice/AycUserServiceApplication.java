b22e57a2133dff6b32b88793bdd97cd644580a7c
Sautino <marcelsauter9299@gmail.com>
2021-01-04 11:42:01 +0100

    Implement Keycloak-Security to secure user-service

--- src/main/java/com/marcelsauter/aycuserservice/AycUserServiceApplication.java
+++ src/main/java/com/marcelsauter/aycuserservice/AycUserServiceApplication.java
@@ -1,9 +1,11 @@
 package com.marcelsauter.aycuserservice;
 
+import com.ayc.keycloaksecurity.config.EnableKeycloakSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableKeycloakSecurity
 public class AycUserServiceApplication {
 
 	public static void main(String[] args) {
